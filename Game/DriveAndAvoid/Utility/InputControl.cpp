2128cd284163a5ff6a34b1fdf05ebf300b49bbc4
korinata <138739456+korinata@users.noreply.github.com>
2024-02-06 15:01:47 +0900

    プログラム書いた
    プログラム書いた

--- Game/DriveAndAvoid/Utility/InputControl.cpp
+++ Game/DriveAndAvoid/Utility/InputControl.cpp
@@ -32,7 +32,7 @@ void InputControl::Update()
 	}
 	else
 	{
-		stick[0].x = ((float)input_state.ThumbLX / (float)SHRT_MIN);
+		stick[0].x = -((float)input_state.ThumbLX / (float)SHRT_MIN);
 	}
 	if (input_state.ThumbLY > 0.0f)
 	{
@@ -40,25 +40,25 @@ void InputControl::Update()
 	}
 	else
 	{
-		stick[0].y = ((float)input_state.ThumbLY / (float)SHRT_MIN);
+		stick[0].y = -((float)input_state.ThumbLY / (float)SHRT_MIN);
 	}
 
 	//右スティック入力値の更新(-1.0f～1.0fに範囲を制限する)
 	if (input_state.ThumbRX > 0.0f)
 	{
-		stick[0].x = (float)input_state.ThumbRX / (float)SHRT_MAX;
+		stick[1].x = (float)input_state.ThumbRX / (float)SHRT_MAX;
 	}
 	else
 	{
-		stick[0].x = ((float)input_state.ThumbRX / (float)SHRT_MIN);
+		stick[1].x = -((float)input_state.ThumbRX / (float)SHRT_MIN);
 	}
-	if (input_state.ThumbLY > 0.0f)
+	if (input_state.ThumbRY > 0.0f)
 	{
-		stick[0].y = (float)input_state.ThumbRY / (float)SHRT_MAX;
+		stick[1].y = (float)input_state.ThumbRY / (float)SHRT_MAX;
 	}
 	else
 	{
-		stick[0].y = ((float)input_state.ThumbRY / (float)SHRT_MIN);
+		stick[1].y = -((float)input_state.ThumbRY / (float)SHRT_MIN);
 	}
 }
 
--- Game/DriveAndAvoid/Utility/Vector2D.h
+++ Game/DriveAndAvoid/Utility/Vector2D.h
@@ -8,15 +8,15 @@ public:
 	float y;
 
 public:
-	Vector2D() :x(0.0f), y(0.0f)
+	Vector2D() : x(0.0f), y(0.0f)
 	{
 
 	}
-	Vector2D(float scalar) :x(scalar), y(scalar)
+	Vector2D(float scalar) : x(scalar), y(scalar)
 	{
 
 	}
-	Vector2D(float mx, float my) :x(mx), y(my)
+	Vector2D(float mx, float my) : x(mx), y(my)
 	{
 
 	}
